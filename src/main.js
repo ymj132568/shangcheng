0921d0c07ac54febad1048d351ec3cd9e4f19d16
ymj132568 <69382176+ymj132568@users.noreply.github.com>
2020-09-11 16:01:35 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -38,28 +37,28 @@ router.beforeEach((to, from, next) => {
 
 import VuePreview from 'vue-preview'
 Vue.use(VuePreview)
-// 添加请求拦截器
+
 axios.interceptors.request.use(function (config) {
-    // 在发送请求之前做些什么
-    // this.loding = true
+   
+
 
     return config;
   }, function (error) {
-    // 对请求错误做些什么
+  
     return Promise.reject(error);
   });
-// 添加响应拦截器
+
 axios.interceptors.response.use(function (response) {
-    // 对响应数据做点什么
+   
 
     return response;
   }, function (error) {
-    // 对响应错误做点什么
+   
     return Promise.reject(error);
   });
 
 
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
